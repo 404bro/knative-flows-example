6144da25f7b1a1c742dabdebe2d0b280f01c1ff5
Yuezheng Liu <i@lyz.plus>
2024-03-01 12:49:12 +0000

    sh & workspace

--- clean.sh
+++ clean.sh
@@ -2,12 +2,12 @@
 
 export REGISTRY=YOUR_REGISTRY # Container registry + registry namespace. (ex 'ghcr.io/myuser').
 
-functions=("op/add1" "op/div2" "op/mul3" "filter/is-even" "filter/is-odd" "util/event-sender")
+functions=("add1" "div2" "mul3" "is-even" "is-odd" "event-sender")
 for function in "${functions[@]}"; do
-    func deploy --path ./func/$function --registry $REGISTRY
+    func delete $function
 done
 
 resources=("resources/event-display.yaml" "resources/parallel.yaml" "resources/sequence.yaml")
 for resource in "${resources[@]}"; do
-    kubectl create -f $resource
+    kubectl delete -f $resource --ignore-not-found=true
 done
